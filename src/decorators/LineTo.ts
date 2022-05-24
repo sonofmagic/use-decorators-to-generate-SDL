8d79a696e85202ece22b2cb4eab312e6532cc41e
SonOfMagic <qq1324318532@gmail.com>
2022-05-24 14:09:48 +0800

    feat: commit codes

--- src/decorators/LineTo.ts
+++ src/decorators/LineTo.ts
@@ -4,16 +4,26 @@ const LineToMetadataKey = Symbol('LineTo')
 export const defaultArrow = '-->'
 
 export interface LineToOptions {
-  to?: string
+  to: string
   text?: string
 }
 
-export function getLineTo (target: any, propertyKey: string | symbol) {
+export function renderLineToStr (optionsArray: LineToOptions[]): string[] {
+  return optionsArray.map((options) => {
+    const { to, text } = options
+    return `${defaultArrow}${text ? `|${text}|` : ''}${to}`
+  })
+}
+
+export function getLineTo (
+  target: any,
+  propertyKey: string | symbol
+): LineToOptions[] | undefined {
   return Reflect.getMetadata(LineToMetadataKey, target, propertyKey)
 }
 
-export function LineTo (options: LineToOptions = {}) {
-  const opt = defu<LineToOptions, LineToOptions>(options, {})
+export function LineTo (options: LineToOptions) {
+  const opt = defu<LineToOptions, Partial<LineToOptions>>(options, {})
   if (opt.to === undefined) {
     throw new TypeError('LineTo decorator requires a `to` property')
   }
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,25 +1,31 @@
 import { getNode, renderNodeStr } from '@/decorators/Node'
-import { getLineTo } from '@/decorators/LineTo'
+import { getLineTo, renderLineToStr } from '@/decorators/LineTo'
 
 export class Renderer {
   render (instance: Object): string {
-    console.log(instance)
-    const res = []
+    const res: string[] = []
     // @ts-ignore
-    res.push([instance.type, instance.direction])
-    const keys = Reflect.ownKeys(instance).filter((k) => typeof k === 'string') as string[]
+    res.push([instance.type, instance.direction].join(' '))
+    const keys = Reflect.ownKeys(instance).filter(
+      (k) => typeof k === 'string'
+    ) as string[]
     for (const key of keys) {
-      let str
+      let str: string = ''
       const node = getNode(instance, key)
       if (node) {
         str += renderNodeStr(key, node)
       }
       const lineTo = getLineTo(instance, key)
-      if (lineTo) {
-        res.push(lineTo)
+      if (Array.isArray(lineTo)) {
+        const lineStrs = renderLineToStr(lineTo)
+        lineStrs.forEach((x) => {
+          res.push(str + x)
+        })
+      } else {
+        res.push(str)
       }
     }
-    console.log(keys)
-    return ''
+
+    return res.join('\n')
   }
 }
